8d6257cc484bf5fcb99056a2fe58cfef0cba3172
Gbhere900 <1843123351@qq.com>
2025-03-10 18:38:19 +0800

    Compeleted move
    实现了新控制系统实现玩家移动

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-   private Rigidbody rigidbody;
-    void Start()
+    private PlayerInputControl playerInputControl;
+    private Rigidbody rigidbody;
+
+    [SerializeField] Vector2 inputDirection;
+    [SerializeField] float yVelocity;
+    void Awake()
     {
+        playerInputControl = new PlayerInputControl();  
         rigidbody = GetComponent<Rigidbody>();
         rigidbody.velocity = Vector3.right;
     }
 
-    
+    private void OnEnable()
+    {
+        playerInputControl.Enable();
+    }
     void Update()
     {
-        
+        inputDirection = playerInputControl.Player.Move.ReadValue<Vector2>();
+    }
+
+    private void OnDisable()
+    {
+        playerInputControl.Disable();
     }
 }
