29b68b6b024426af868d0a27cd4379297c45bb4c
Gbhere900 <1843123351@qq.com>
2025-03-11 11:05:55 +0800

    Camera constraints

--- Assets/Script/CameraControl.cs
+++ Assets/Script/CameraControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,15 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     [SerializeField] private Transform transfromToFollow;
+
     private Vector3 cameraTransfrom;
+
+    public Transform constraint;
     private void LateUpdate()
     {
-        cameraTransfrom = transfromToFollow.transform.position;
+        
+        cameraTransfrom.x = Math.Clamp(transfromToFollow.position.x, -constraint.position.x, constraint.position.x);
+        cameraTransfrom.z = Math.Clamp(transfromToFollow.position.z, -constraint.position.z, constraint.position.z);
         cameraTransfrom.y = 30;
         this.transform.position = cameraTransfrom;
     }
