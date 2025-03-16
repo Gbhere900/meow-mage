a0ac4a699887b2f30f7974eed065501cd06b14cf
Gbhere900 <1843123351@qq.com>
2025-03-16 20:36:07 +0800

    healthText
    血条文本

--- Assets/Script/Player/PlayerHealth.cs
+++ Assets/Script/Player/PlayerHealth.cs
@@ -3,22 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 100;
     [SerializeField] private float health;
-    [SerializeField] private Slider healthSlider;
 
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private TextMeshProUGUI healthText;
     private void Awake()
     {
         health = maxHealth;
-        healthSlider.value = health / maxHealth;
+        UpdateHealthUI();
     }
 
     public void ReceiverDamage(float damage)
     {
         health -= Math.Min(health, damage);
-        healthSlider.value = health / maxHealth;
+        UpdateHealthUI();
+        if (health == 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+        
     }
-    
+ 
+    private void UpdateHealthUI()
+    {
+        healthSlider.value = health / maxHealth;
+        healthText.text = health + " / " + maxHealth;
+    }
 }
