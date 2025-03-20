5a1b9efba9875a150cda51561680edc9911af54d
Gbhere900 <1843123351@qq.com>
2025-03-20 11:51:08 +0800

    create bullet prefebs + enemy health

--- Assets/Script/Enemy/Common/EnemyHealth.cs
+++ Assets/Script/Enemy/Common/EnemyHealth.cs
@@ -1,14 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
+[RequireComponent(typeof(DamageUI))]
 public class EnemyHealth : MonoBehaviour
 {
+    [Header("数值")]
+    [SerializeField] private float maxHealth = 10;
+    [SerializeField] private float health = 10;
+
+    [Header("粒子效果")]
     [SerializeField] private ParticleSystem VFX_passaway;
+    private DamageUI damageUI;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        damageUI = GetComponent<DamageUI>();
+    }
     void Start()
     {
-        
+        health = maxHealth;
     }
 
     // Update is called once per frame
@@ -17,6 +30,15 @@ public class EnemyHealth : MonoBehaviour
         
     }
 
+    void ReceiveDamage(float damage)
+    {
+        health -= Math.Min(health, damage);
+        damageUI.PlayDamageUIAnimation();
+        if (health == 0)
+        {
+            PassAway();
+        }
+    }
     public void PassAway()
     {
         VFX_passaway.transform.parent = null;
