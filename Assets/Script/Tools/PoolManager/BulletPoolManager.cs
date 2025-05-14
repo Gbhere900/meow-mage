40946056d1b34b5dbbd7ce450a84d91f037f3ec7
Gbhere900 <1843123351@qq.com>
2025-05-14 17:44:12 +0800

    version2.0

--- Assets/Script/Tools/PoolManager/BulletPoolManager.cs
+++ Assets/Script/Tools/PoolManager/BulletPoolManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Pool;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
 [System.Serializable]
 public struct Pair
 {
